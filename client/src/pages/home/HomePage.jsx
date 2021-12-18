738b431cd51afe07ab139be63a53264a6da842ad
Daniel Binoy <dbinoy15@gmail.com>
2021-12-18 03:55:25 -0800

    started client

---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+        <h1 className="d-title text-center">
+            Scum Wrapped 2021
+        </h1>
+    </div>
+  );
+}
+
+export default HomePage;
