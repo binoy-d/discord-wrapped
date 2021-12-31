0110aa1216aaf9944b8df8dce8209c3d6bf4b72c
Daniel Binoy <dbinoy15@gmail.com>
2021-12-31 05:36:00 -0800

    idek anymore

--- client/src/pages/landing/LandingPage.jsx
+++ client/src/pages/landing/LandingPage.jsx
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './LandingPage.css';
 import { filler_messages } from "../../data";
 
 
-const MessageBubble = ({msg}) => 
-    <div className="d-message-bubble">
-        <p>{msg.content}</p>
-        <p>{msg.x}, {msg.y}</p>
+const MessageBubble = ({ msg, scale}) =>
+    <div className="d-message-bubble float" style={{
+        position: "absolute",
+        left: msg.x +"vw",
+        top: msg.y +"vh",
+    }}>
+        <p className="d-bubble-text">{msg.content}</p>
     </div>
 
 function LandingPage() {
@@ -14,43 +17,30 @@ function LandingPage() {
     const numMessages = 10;
     const [messages, setMessages] = useState([]);
 
-    const updateMessagePositions = () => {
-        if(messages.length === 0) return;
-        const updated_messages = messages.map((msg) => {
-            return {
-                content: msg.content, //xd
-                x: msg.x+Math.random()-0.5,
-                y: msg.y+Math.random()-0.5
-            }
-        });
-        setMessages(updated_messages);
-    }
-
-    useEffect(()=>{
+    useEffect(() => {
         //get numMessages random messages from filler messages
         //replace this with something fancy later
         const sample = filler_messages.sort(() => 0.5 - Math.random()).slice(0, numMessages);
-        const transformed_sample = sample.map((msg) => { 
+        const transformed_sample = sample.map((msg) => {
             return {
                 content: msg,
                 x: Math.random() * 50 + 50,
-                y: Math.random() * 50 + 50
+                y: Math.random() * 100
             }
         })
         setMessages(transformed_sample);
-        setInterval(updateMessagePositions, 100);
-    }, [])
+    }, []);
 
     return (
         <div className="LandingPage">
             <h1 className=" landing-title ">
-                It's been an <br /> 
+                It's been an <br />
                 <strong>interesting year</strong>
                 <br /> for the {serverName} <br />
                 Server
             </h1>
-            {messages.map((msg, i) => 
-            <MessageBubble key = {"message-bubble-"+i} msg={msg}/>
+            {messages.map((msg, i) =>
+                <MessageBubble key={"message-bubble-" + i} msg={msg} scale={(numMessages - i) / (numMessages/2)} />
             )}
         </div>
     );
