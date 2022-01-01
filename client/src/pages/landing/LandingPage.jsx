9dadfdb87d25aa18b6e092a97362154e2bc223f3
Daniel Binoy <dbinoy15@gmail.com>
2022-01-01 12:54:00 -0800

    bubbles looking better

--- client/src/pages/landing/LandingPage.jsx
+++ client/src/pages/landing/LandingPage.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import './LandingPage.css';
 import { filler_messages } from "../../data";
+import {FaDiscord} from "react-icons/fa"
 
-
-const MessageBubble = ({ msg, scale}) =>
+const MessageBubble = ({ msg, x, y, scale, i}) =>
     <div className="d-message-bubble float" style={{
         position: "absolute",
-        left: msg.x +"vw",
-        top: msg.y +"vh",
+        left: x +"vw",
+        top: y +"vh",
+        animationDuration: 8/scale+"s"
     }}>
-        <p className="d-bubble-text">{msg.content}</p>
+        <FaDiscord size={20+scale*20} className="message-discord-icon"  />
+        <p className="d-bubble-text" style={{fontSize:scale+"em"}}>"{msg}"</p>
         <div className="d-bubble-background"></div>
     </div>
 
@@ -22,14 +24,7 @@ function LandingPage() {
         //get numMessages random messages from filler messages
         //replace this with something fancy later
         const sample = filler_messages.sort(() => 0.5 - Math.random()).slice(0, numMessages);
-        const transformed_sample = sample.map((msg) => {
-            return {
-                content: msg,
-                x: Math.random() * 50 + 50,
-                y: Math.random() * 100
-            }
-        })
-        setMessages(transformed_sample);
+        setMessages(sample);
     }, []);
 
     return (
@@ -40,9 +35,11 @@ function LandingPage() {
                 <br /> for the {serverName} <br />
                 Server
             </h1>
-            {messages.map((msg, i) =>
-                <MessageBubble key={"message-bubble-" + i} msg={msg} scale={(numMessages - i) / (numMessages/2)} />
-            )}
+            <MessageBubble i={0} msg={messages[0]} scale={2.5} x={30} y = {45} />
+            <MessageBubble i={0} msg={messages[1]} scale={1.5} x={70} y = {10} />
+            <MessageBubble i={0} msg={messages[2]} scale={1} x={40} y = {20} />
+            <MessageBubble i={0} msg={messages[3]} scale={1} x={60} y = {80} />
+            
         </div>
     );
 }
