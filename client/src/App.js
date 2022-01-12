400b55cc49073be7b6116a5aaee9c2853c55f977
Daniel Binoy <dbinoy15@gmail.com>
2022-01-12 00:34:48 -0800

    add message count

--- client/src/App.js
+++ client/src/App.js
@@ -6,37 +6,36 @@ import ChannelPage from './pages/channel/ChannelPage';
 import MemberPage from './pages/member/MemberPage';
 import ChannelStatsPage from './pages/channelstats/ChannelStatsPage';
 import MemberStatsPage from './pages/memberstats/MemberStatsPage';
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet} from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<PageLayout />}>
-          <Route index element={<LandingPage />} />
-          
-        <Route path="/home" element={<MainPage />}></Route>
-          <Route path="/channels/:id" element={<ChannelPage />} />
-          <Route path="/members/:id" element={<MemberPage />} />
-          <Route path="channels" element={<ChannelStatsPage />} />
-          <Route path="members" element={<MemberStatsPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<PageLayout/>}>
+        <Route index element={<LandingPage />} />
+        <Route path="/home" element={<MainPage  />}></Route>
+        <Route path="/channels/:id" element={<ChannelPage  />} />
+        <Route path="/members/:id" element={<MemberPage  />} />
+        <Route path="channels" element={<ChannelStatsPage  />} />
+        <Route path="members" element={<MemberStatsPage  />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
 
-function PageLayout() {
+function PageLayout(props) {
   return (
     <div className="page-root">
       <Sidebar />
-      <Outlet />
+      <Outlet {...props}/>
     </div>
   );
 }
 
 
-function PageNotFound(){
+function PageNotFound() {
   return (
     <h1>Page Not Found!! Fuck off.</h1>
   );
--- client/src/pages/channel/ChannelPage.jsx
+++ client/src/pages/channel/ChannelPage.jsx
@@ -1,14 +1,32 @@
 import './ChannelPage.css';
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import FadeIn from 'react-fade-in/lib/FadeIn';
 
 function ChannelPage() {
   let params = useParams();
+  const [messageCount, setMessageCount] = useState(0)
+  useEffect(() => {
+    axios.get(`http://localhost:5000/channel/${params.id}/messagecount`)
+      .then((response) => {
+        if ("num_messages" in response.data) {
+          setMessageCount(response.data.num_messages);
+        }
+      })
+  }, [params.id]);
+
   return (
     <div className="ChannelPage">
-        <h1 className="d-title text-center mt-3">
-            Channel Page
-        </h1>
-        <h2 className="text-center">Show statistics about {params.id}</h2>
+      <h1 className="d-title text-center mt-5">
+        #{params.id}
+      </h1>
+      <FadeIn>
+        <h2 className='channel-tagline'> <strong>Holy smokes!</strong> You sent<br />
+        <span className="message-count"><span className='glow'>{messageCount}</span> messages</span><br />
+        in #{params.id} this year!</h2>
+      </FadeIn>
+      
     </div>
   );
 }
