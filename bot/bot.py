1c2cd7624ee7816acbe664e0cfb94dd0014d0f82
Evan Nguyen <evannguyen1101@yahoo.com>
2022-01-17 16:42:56 -0800

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -37,7 +37,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix = get_prefix)
-DiscordComponents(bot) #Enables use of third party components library. Will be removed when discord.py 2.0 releases.
+ComponentsBot(bot) #Enables use of third party components library. Will be removed when discord.py 2.0 releases.
 #------------------------------------------------------------------
 
 async def scan(message):
