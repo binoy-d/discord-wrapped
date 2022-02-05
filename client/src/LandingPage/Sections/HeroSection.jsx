e3f117ddac91fd402d4f632f994219729f11f175
Daniel Binoy <dbinoy15@gmail.com>
2022-02-04 20:05:30 -0800

    yeet

--- client/src/LandingPage/Sections/HeroSection.jsx
+++ client/src/LandingPage/Sections/HeroSection.jsx
@@ -1,5 +1,7 @@
 import AddToDiscordButton from "./AddToDiscordButton";
 import Marquee from "react-fast-marquee";
+import Gem_Emoji from "./Images/Gem_Emoji.png"
+import Loudspeaker_Emoji from "./Images/Loudspeaker_Emoji.png"
 
 function HeroSection() {
     return (
@@ -48,16 +50,16 @@ function HeroSection() {
             <div className="braincells-container">
                 <Marquee speed={50}>
                     <span className="braincells-box">
-                        <p>Scroll to lose brain cells 📣</p>
+                        <p>Scroll to lose brain cells <img className="braincell-emoji" src={Gem_Emoji} /></p>
                     </span>
                     <span className="braincells-box">
-                        <p>Scroll to lose brain cells 💎</p>
+                        <p>Scroll to lose brain cells <img className="braincell-emoji" src={Loudspeaker_Emoji} /></p>
                     </span>
                     <span className="braincells-box">
-                        <p>Scroll to lose brain cells 📣</p>
+                        <p>Scroll to lose brain cells <img className="braincell-emoji" src={Gem_Emoji} /></p>
                     </span>
                     <span className="braincells-box">
-                        <p>Scroll to lose brain cells 💎</p>
+                        <p>Scroll to lose brain cells <img className="braincell-emoji" src={Loudspeaker_Emoji} /></p>
                     </span>
                 </Marquee>
 
